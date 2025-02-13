2f21a4af0de60b61d2ffa5030fab17bcab146838
前端小然子 <xiaoranzife@163.com>
2025-02-13 15:23:23 +0800

    feat: test location info

--- src/main.tsx
+++ src/main.tsx
@@ -14,28 +14,6 @@ console.log("当前页面信息:", {
   hash: window.location.hash
 });
 
-// 拦截页面跳转
-const originalPushState = window.history.pushState;
-const originalReplaceState = window.history.replaceState;
-
-window.history.pushState = function(...args) {
-  console.log("pushState 跳转:", args);
-  return originalPushState.apply(this, args);
-};
-
-window.history.replaceState = function(...args) {
-  console.log("replaceState 跳转:", args);
-  return originalReplaceState.apply(this, args);
-};
-
-// 监听 popstate 事件
-window.addEventListener('popstate', (event) => {
-  console.log("popstate 事件:", {
-    state: event.state,
-    location: window.location.href
-  });
-});
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Providers>
